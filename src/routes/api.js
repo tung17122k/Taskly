e430c6624cfc57f68e6e8481ad58bdcf42d68f86
tung17122k <sanking1712@gmail.com>
2025-04-16 11:45:12 +0700

    update and delete user

--- src/routes/api.js
+++ src/routes/api.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { createUser, handleLogin, handleRefreshToken, getUser, getAccount } = require('../controllers/userController');
+const { createUser, handleLogin, handleRefreshToken, getUser, getAccount, putUpdateUser, deleteUser } = require('../controllers/userController');
 const auth = require('../middleware/auth');
 
 const routerAPI = express.Router();
@@ -10,6 +10,10 @@ routerAPI.all("*", auth)
 routerAPI.post('/register', createUser)
 
 routerAPI.get('/user', getUser)
+
+routerAPI.put('/user', putUpdateUser)
+
+routerAPI.delete('/user', deleteUser)
 
 routerAPI.post('/login', handleLogin)
 
